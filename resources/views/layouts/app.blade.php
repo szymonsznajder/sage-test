dde83117a843658c64752fbb576916247e5ba88f
szymonsznajder <sznajder.szymon@gmail.com>
2021-01-18 18:31:57 +0100

    all files

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html {!! get_language_attributes() !!}>
+  @include('partials.head')
+  <body @php body_class() @endphp>
+    @php do_action('get_header') @endphp
+    @include('partials.header')
+    <div class="wrap container" role="document">
+      <div class="content">
+      <div class="content-szymon">
+        <main class="main">
+          @yield('content')
+        </main>
+        @if (App\display_sidebar())
+          <aside class="sidebar">
+            @include('partials.sidebar')
+          </aside>
+        @endif
+      </div>
+      </div>
+    </div>
+    @php do_action('get_footer') @endphp
+    @include('partials.footer')
+    @php wp_footer() @endphp
+  </body>
+</html>
