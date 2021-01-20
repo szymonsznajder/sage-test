2e9fc4b94c474018082517c9acc8d74452abcb53
szymonsznajder <sznajder.szymon@gmail.com>
2021-01-20 23:59:22 +0100

    some files with header

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,20 +4,22 @@
   <body @php body_class() @endphp>
     @php do_action('get_header') @endphp
     @include('partials.header')
-    <div class="wrap container" role="document">
-      <div class="content">
-      <div class="content-szymon">
-        <main class="main">
-          @yield('content')
-        </main>
-        @if (App\display_sidebar())
-          <aside class="sidebar">
-            @include('partials.sidebar')
-          </aside>
-        @endif
+    <div class="main-wrapper">
+      <div class="wrap container" role="document">
+        <div class="content">
+        <div class="content-szymon">
+          <main class="main">
+            @yield('content')
+          </main>
+          @if (App\display_sidebar())
+            <aside class="sidebar">
+              @include('partials.sidebar')
+            </aside>
+          @endif
+        </div>
+        </div>
       </div>
-      </div>
-    </div>
+    </div> 
     @php do_action('get_footer') @endphp
     @include('partials.footer')
     @php wp_footer() @endphp
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,7 +1,10 @@
 <header class="banner">
   <div class="container">
     <div class="logo">
-      <a class="brand" href="{{ home_url('/') }}">{{ get_bloginfo('name', 'display') }}</a>
+      <a class="brand" href="{{ home_url('/') }}">
+        <img src="@asset('images/SupermanPNG.png')" alt="Superman">
+        {{-- {{ get_bloginfo('name', 'display') }} --}}
+      </a>
     </div>
     <nav class="nav-primary">
       @if (has_nav_menu('primary_navigation'))
